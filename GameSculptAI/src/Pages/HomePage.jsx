8bd8a01b0df9ba08517d14a249765b20224721dc
Justinscitech <zszombiesize@gmail.com>
2023-11-24 23:52:51 -0500

    init homepage

--- GameSculptAI/src/Pages/HomePage.jsx
+++ GameSculptAI/src/Pages/HomePage.jsx
@@ -18,8 +18,14 @@ function HomePage() {
         <div className="relative flex place-items-center flex-col z-[-1] before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
           <h1 className="mb-3 text-4xl font-bold">
             <AnimatedText
-              sentence="Create Your Next Game With GameSculptAI"
+              sentence="Create Your Next Game With"
               styling="bg-gradient-to-b from-cyan-500 via-blue-500 to-purple-500 bg-clip-text text-transparent"
+            />
+          </h1>
+          <h1 className="mb-3 text-4xl font-bold">
+            <AnimatedText
+              sentence="GameSculptAI"
+              styling="text-white"
             />
           </h1>
         </div>
