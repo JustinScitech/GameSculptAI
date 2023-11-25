bd6666b254631a2ea3f09f59a2fa70ab33584536
Justinscitech <zszombiesize@gmail.com>
2023-11-25 13:15:09 -0500

    update creation page

--- GameSculptAI/src/Pages/HomePage.jsx
+++ GameSculptAI/src/Pages/HomePage.jsx
@@ -42,7 +42,6 @@ function HomePage() {
         <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
   GET STARTED
         </button>
-        </div>
         </Link>
         <motion.div
           className="flex"
